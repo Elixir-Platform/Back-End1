3c263bf52942052fd4f931e136cad1b5a57e1162
NourAldahan <nooraladhan@gmail.com>
2022-12-12 17:36:15 +0200

    function register and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-
 use Illuminate\Http\Response;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -32,38 +31,9 @@ class AuthController extends Controller
 
     }
     public function logout(Request $request){
-        auth()->user()->tokens()->delete();
+        auth('sanctum')->user()->tokens()->delete();
         return [
             'message' =>'logged out'
         ];
     }
-    public function login(Request $request){
-        $fields =$request->validate([
-           
-            'email'=>'required|string',
-            'password'=>'required|string'
-        ]);
-        
-        //check email 
-        $user =User::where('email',$fields['email'])->first();
-        //check password
-        if(!$user ||!Hash::check($fields['password'], $user->password)){
-            return response([
-                'message'=>'return the password'
-            ],401);
-        }
-       
-
-        
-       
-        $token =$user->createToken('myapptoken')->plainTextToken;
-        $response =[
-            'user'=>$user ,
-            'token'=>$token 
-        ];
-        return response($response,201);
-
-    }
-
 }
-
